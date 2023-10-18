5f79a23c5ee0de3d82f57c7de882c8344b9234a1
jp-sanchezs <pablosanchez19@estudiantec.cr>
2023-10-17 19:02:38 -0600

    CREATE
    Puestos y Depertamentos

---
+++ SQLfiles/Departamentos.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO 
+Departamentos (Id, Nombre)
+VALUES 
+  (1, 'Enlaminado')
+, (2, 'Laboratorio')
+, (3, 'Bodega de materiales')
+, (4, 'Bodega de producto terminado');
+
+SELECT * FROM dbo.Departamentos
---
+++ SQLfiles/Puestos.sql
@@ -0,0 +1,11 @@
+INSERT INTO Puestos (Nombre, SalarioXHora)
+VALUES
+    ('Electricista', 1200.00)
+	, ('Auxiliar de Laboratorio', 1250.00)
+	, ('Operador de Máquina', 1025.00)
+	, ('Técnico de Mantenimiento', 1300.00)
+	, ('Ingeniero de Software', 1500.00);
+
+SELECT * FROM dbo.Puestos
+
+
