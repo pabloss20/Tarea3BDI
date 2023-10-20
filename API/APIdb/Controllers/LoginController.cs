a301bdafcaae615ac0506dfb8095dd4e5f8d710d
jp-sanchezs <pablosanchez19@estudiantec.cr>
2023-10-20 08:30:25 -0600

    CREATE
    Controlador de la API para el Login (ValidarEmpleado SP)

--- API/APIdb/Controllers/LoginController.cs
+++ API/APIdb/Controllers/LoginController.cs
@@ -2,8 +2,9 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using System.Data.SqlClient;
+using System;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace APIdb.Controllers
 {
@@ -18,7 +19,6 @@ namespace APIdb.Controllers
             _configuration = configuration;
         }
 
-        // POST el cual se usa para validar un usuario
         [HttpPost]
         [Route("Login")]
         public IActionResult Login([FromBody] Login login)
@@ -28,51 +28,62 @@ namespace APIdb.Controllers
                 var clientIpAddress = HttpContext.Connection.RemoteIpAddress;
                 var ip = clientIpAddress.ToString();
 
-                // Conexión en appsettings.json
                 using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("connection").ToString()))
                 {
-                    // Inicia la conexión
                     connection.Open();
-                    // Se llama al SP (nombre de SP, conexión)
-                    SqlCommand command = new SqlCommand("ValidarUsuario", connection);
+
+                    SqlCommand command = new SqlCommand("ValidarEmpleado", connection);
                     command.CommandType = CommandType.StoredProcedure;
 
-                    // Configura los parámetros de entrada (BD, Modelo)
                     command.Parameters.AddWithValue("@Username", login.Username);
                     command.Parameters.AddWithValue("@Password", login.Password);
-                    command.Parameters.AddWithValue("@PostIP", ip);
 
                     // Configura los parámetros de salida
-                    SqlParameter autenticadoParam = new SqlParameter("@Registrado", SqlDbType.Bit);
-                    autenticadoParam.Direction = ParameterDirection.Output;
-                    command.Parameters.Add(autenticadoParam);
+                    SqlParameter resultadoParam = new SqlParameter("@Resultado", SqlDbType.Bit);
+                    resultadoParam.Direction = ParameterDirection.Output;
+                    command.Parameters.Add(resultadoParam);
 
-                    SqlParameter messageParam = new SqlParameter("@Message", SqlDbType.VarChar, 100);
-                    messageParam.Direction = ParameterDirection.Output;
-                    command.Parameters.Add(messageParam);
+                    SqlParameter mensajeParam = new SqlParameter("@Mensaje", SqlDbType.VarChar, -1);
+                    mensajeParam.Direction = ParameterDirection.Output;
+                    command.Parameters.Add(mensajeParam);
 
-                    // Ejecuta el SP
+                    SqlParameter tipoParam = new SqlParameter("@Tipo", SqlDbType.Int);
+                    tipoParam.Direction = ParameterDirection.Output;
+                    command.Parameters.Add(tipoParam);
+
+                    SqlParameter tipoIdEmpleadoParam = new SqlParameter("@TipoIdEmpleado", SqlDbType.Int);
+                    tipoIdEmpleadoParam.Direction = ParameterDirection.Output;
+                    command.Parameters.Add(tipoIdEmpleadoParam);
+
                     command.ExecuteNonQuery();
 
-                    // Recupera los valores de los parámetros de salida
-                    bool autenticado = (bool)autenticadoParam.Value;
-                    string message = messageParam.Value.ToString();
+                    bool resultado = (bool)resultadoParam.Value;
+                    string mensaje = mensajeParam.Value.ToString();
+                    int tipo = (int)tipoParam.Value;
+                    int tipoIdEmpleado = (int)tipoIdEmpleadoParam.Value;
 
-                    // Crea una instancia de tu modelo de respuesta
-                    var response = new Response
+                    if (resultado)
                     {
-                        statusCode = autenticado,
-                        statusMessage = message
-                        // Puedes agregar más propiedades según sea necesario
-                    };
-
-
-                    return Ok(response);
+                        return Ok(new
+                        {
+                            Resultado = resultado,
+                            Mensaje = mensaje,
+                            Tipo = tipo,
+                            TipoIdEmpleado = tipoIdEmpleado
+                        });
+                    }
+                    else
+                    {
+                        return BadRequest(new
+                        {
+                            Resultado = resultado,
+                            Mensaje = mensaje
+                        });
+                    }
                 }
             }
             catch (Exception ex)
             {
-                // Maneja errores aquí
                 return StatusCode(500, "Error al autenticar: " + ex.Message);
             }
         }
